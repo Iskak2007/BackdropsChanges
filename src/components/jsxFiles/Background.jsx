eaa63d38d44d6c374f2f41029ab032954eb004c3
iskak2007 <isaak9894@gmail.com>
2024-06-20 23:02:59 +0600

    update all

--- src/components/jsxFiles/Background.jsx
+++ src/components/jsxFiles/Background.jsx
@@ -1,4 +1,3 @@
-import { motion } from "framer-motion"
 import bycicle from '../../assets/bycicle.svg'
 import background from '../../assets/background.svg'
 import ocean from '../../assets/ocean.svg'
@@ -13,21 +12,19 @@ function ChangeBG() {
 
     return(
         <>
-            <div className="flex w-svw-100 h-svh-100 justify-center" onClick={onHandleClick}>
-             <div className="w-full justify-center mt-[5%]">  
-                  <div className="h-full mt-10 h-svh-100 flex ml-32 flex-col flex-wrap">
-                    <h1 className="text-4xl ">Thematic Backgrounds</h1>
+            <div className="flex justify-center" >
+             <div className="w-full justify-center">  
+                  <div className="h-full mt-10 flex ml-32 flex-col">
+                    <h1 className="text-4xl font-bold">Thematic Backgrounds</h1>
                     <h2 className="text-xl text-gray-500">Whimsical handcrafted backdrops in different styles</h2>
                 </div>
-                <div  className="w-full flex justify-center h-full">
-                <img className='w-96 flex justify-center absolute' src={backdrop} alt="" />
-                <img className='w-96 z-20 flex justify-center absolute' src={bycicle} alt="" />
-
+                <div  className="w-full flex justify-center h-full" onClick={onHandleClick}>
+                <img className='w-[60%] h-[80%] flex justify-center absolute' src={backdrop} alt="" />
+                <img className='w-96 translate-y-20 flex justify-center absolute' src={bycicle} alt="" />
                 </div>
-
                 </div>
             </div>
-            {click ?  <div  className='flex rounded-lg flex-col fixed gap-3 p-5 right-32 top-32 border border-black'>
+            {click ?  <div  className='flex rounded-lg flex-col shadow-xl fixed gap-3 p-5 right-32 top-32 border bg-white border-black'>
                     <img onClick={() => setBackdrop(background)} className="w-full" src={background} alt="" />
                     <img  onClick={() => setBackdrop(ocean)} className="w-full" src={ocean} alt="" />
                 </div> : "" }
